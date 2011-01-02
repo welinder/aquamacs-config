8a29a7627f038c5249485680e24863038c127785
Peter Welinder <peter@welinder.se>
2011-01-02 09:37:36 -0800

    tuned multi-term more

--- custom/multi-term.el
+++ custom/multi-term.el
@@ -5,3 +5,16 @@
 (setq multi-term-program "/bin/bash") ; the colors in zsh doesn't work well
 ;(setq multi-term-program "~/bin/eshell")
 ;(setenv "ESHELL" (expand-file-name "~/bin/eshell"))
+
+;; set colors to match wombat
+(custom-set-variables
+ '(term-default-bg-color "#242424")        ;; background color (black)
+ '(term-default-fg-color "#f6f3e8"))       ;; foreground color (yellow)
+
+;; turn off line highlighting in term mode
+(add-hook 'term-mode-hook 
+	  (lambda()
+      (set (make-local-variable 'global-hl-line-mode) nil)
+	    (message "%s" "This is in term mode and hook enabled.")
+      ))
+
